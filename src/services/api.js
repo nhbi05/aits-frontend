0f607476161cface84ba03ac09c2d4599bc0e1e0
Housnah Nansereko <nhousnahishaq@gmail.com>
2025-04-10 21:34:57 +0300

    added images

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 // src/services/api.js - Complete implementation with JWT authentication
 import axios from 'axios';
 
-const API_URL = 'http://localhost:8000/api';
+const API_URL = 'https://academictrackingsystem-production2.up.railway.app/api';
 
 // Create main API instance
 const api = axios.create({
